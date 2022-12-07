85d145fc966cac1efde1f807a045ed6928fdaa53
Ye Yint <naing.yeyint@bhglobal.com.sg>
2022-12-07 17:38:02 +0800

    Add Department CRUD

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -4,14 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Department extends Model
 {
     use HasFactory;
-
+    protected $table = "departments";
     protected $fillable = ['name', 'description'];
 
-    public function employees()
+    public function employees(): HasMany
     {
         return $this->hasMany(Employee::class);
     }
